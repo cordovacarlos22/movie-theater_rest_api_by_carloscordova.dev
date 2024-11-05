f39ff4cad4915c2806a4d049ab7ce0d239ad58c4
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-05 11:37:04 -0800

    feat: :construction_worker: user controller main stucture.

---
+++ controllers/user.controller.js
@@ -0,0 +1,29 @@
+import User from "../modules/user.model";
+
+// ! CRUD OPERATION
+
+// create a new user 
+const createUser = (req, res) => { 
+
+
+};
+
+// get all users
+const getAllUsers = (req, res) => { };
+
+// get user by id
+const getUserById = (req, res) => { };
+
+// update user by id
+const updateUser = (req, res) => { };
+
+// delete user by id with hard delete operation and soft delete operation
+const deleteUser = (req, res) => { };
+
+export {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+};
