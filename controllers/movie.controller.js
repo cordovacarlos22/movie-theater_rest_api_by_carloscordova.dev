53f1e14eb7ba9b9e700fa1550a941d5defc1e700
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-06 09:40:04 -0800

    feat: :sparkles: add stucture for crud operations needs logic

---
+++ controllers/movie.controller.js
@@ -0,0 +1,59 @@
+import Movie from '../modules/movie.model.js'
+
+// crud operations 
+
+// create a movie 
+const createAMovie = async (req, res) => {
+  try {
+
+  } catch {
+
+  }
+};
+
+// get all movies
+const getAllMovies = async (req, res) => {
+  try {
+
+  } catch (error) {
+
+  }
+};
+// get movie by id 
+const getMovieByMovieId = async (req, res) => {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+// update movie 
+
+const updateMovieById = async (req, res) => {
+  try {
+
+  } catch (error) {
+
+  }
+};
+
+const deleteMovieById = async (req, res) => {
+  try {
+
+  } catch (error) {
+
+  }
+};
+
+
+
+
+
+export {
+  createAMovie,
+  getAllMovies,
+  getMovieByMovieId,
+  updateMovieById,
+  deleteMovieById
+}
