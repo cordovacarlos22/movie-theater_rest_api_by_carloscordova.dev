73aef220bf1268cda1e4cbc51c074d65f77331e6
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-06 10:05:09 -0800

    fix: :truck: fix user path

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,21 +5,21 @@ import * as userController from '../controllers/user.auth.controller.js';
 const userRoutes = express.Router();
 
 // Create a new user
-userRoutes.post('/register', userController.createUser);
+userRoutes.post('/user/register', userController.createUser);
 
 // login user 
-userRoutes.post('/login', userController.loginUser);
+userRoutes.post('/user/login', userController.loginUser);
 
 // Get all users
-userRoutes.get('/', userController.getAllUsers);
+userRoutes.get('/user/', userController.getAllUsers);
 
 // Get user by id 
-userRoutes.get('/:id', userController.getUserById);
+userRoutes.get('/user/:id', userController.getUserById);
 
 // Update user by id
-userRoutes.patch('/:id', userController.updateUser);
+userRoutes.patch('/user/:id', userController.updateUser);
 
 // Delete user by id with hard delete operation and soft delete operation
-userRoutes.delete('/:id', userController.deleteUser);
+userRoutes.delete('/user/:id', userController.deleteUser);
 
 export default userRoutes;
