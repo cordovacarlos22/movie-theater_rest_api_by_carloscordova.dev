bded1bf576b5321e7dd6873953fd04d6aba53d76
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-06 10:05:25 -0800

    feat: :sparkles: add createMovie

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -4,15 +4,50 @@ import Movie from '../modules/movie.model.js'
 
 // create a movie 
 const createAMovie = async (req, res) => {
+
+  // validate movie data
+  const {
+    title,
+    director,
+    release_date,
+    rate, duration,
+    genre,
+    description,
+    price,
+    available_seats,
+    is_available,
+    image_url
+  } = req.body;
+
+  if (!title || !director || !release_date || !rate || !duration || !genre || !description || !price || !available_seats || !is_available || !image_url) {
+    return res.status(400).json({ message: 'All fields are required' });
+  };
+
+  // validate if movie already exists
+  const movieExists = await Movie.findOne({ title });
+  if (movieExists) {
+    return res.status(400).json({ message: 'Movie already exists' });
+  }
+
+  // valida image url is an array 
+  if (!Array.isArray(image_url) || image_url.length === 0) {
+    return res.status(400).json({ message: 'image_url must be an array and not empty' });
+  }
+
+
+  // create new movie
   try {
-
+    const movie = await Movie.create(req.body);
+    res.status(201).json(movie);
   } catch {
-
+    res.status(400).json({ message: 'Failed to create movie', error: error});
   }
 };
 
 // get all movies
 const getAllMovies = async (req, res) => {
+
+  
   try {
 
   } catch (error) {
