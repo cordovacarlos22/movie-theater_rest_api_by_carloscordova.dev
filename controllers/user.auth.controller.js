697b6d80661aed1b99d32cff5cd0f52448840fdd
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-06 09:39:23 -0800

    feat: :truck: rename file

---
+++ controllers/user.auth.controller.js
@@ -0,0 +1,64 @@
+import User from "../modules/user.model";
+
+// ! CRUD OPERATION
+
+// create a new user 
+const createUser = (req, res) => {
+
+  try {
+
+  } catch {
+
+  }
+
+};
+
+// get all users
+const getAllUsers = (req, res) => {
+  try {
+
+  } catch {
+
+  }
+};
+
+// get user by id
+const getUserById = (req, res) => {
+  try {
+
+  } catch {
+
+  }
+};
+
+// login user 
+const loginUser = async () => {
+  
+};
+
+// update user by id
+const updateUser = (req, res) => {
+  try {
+
+  } catch {
+
+  }
+};
+
+// delete user by id with hard delete operation and soft delete operation
+const deleteUser = (req, res) => {
+  try {
+
+  } catch {
+
+  }
+};
+
+export {
+  createUser,
+  loginUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+};
