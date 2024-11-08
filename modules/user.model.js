449f49bfd6de2ad4232c33ee3ab48820eef75a1c
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-08 10:01:51 -0800

    feat: :sparkles: add role of employee

--- modules/user.model.js
+++ modules/user.model.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 // user schema 
 const userSchema = new mongoose.Schema({
-  dni: { type: Number, required: true, unique: true},
+  dni: { type: Number, required: true, unique: true },
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
   dob: { type: Date, required: true }, // format: 'YYYY-MM-DD 
-  role: { type: String, enum: ["admin", "user"], default: "user" },
+  role: { type: String, enum: ["admin", "employee", "user"], default: "user" },
   phone_number: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
